bd26965bfad21a2bca601422cb2d1602340ae031
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2018-06-03 14:55:04 +0700

    Add client script

---
+++ src/client/script/script.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import router from './router';
+import store from './store';
+import app from './vue/app/app.vue';
+import('../style/index.scss');
+
+new Vue({
+    el: '#app',
+    router,
+    store,
+    render: h => h(app, { ref: 'app' }),
+});
