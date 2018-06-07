49b5168607af293d74191aa5f2849e0b1c8f8f67
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2018-06-07 16:52:15 +0700

    Add Routes

--- src/client/script/vue/routes/home.vue
+++ src/client/script/vue/routes/home.vue
@@ -1,7 +1,20 @@
 <template>
-    <div>Home.vue</div>
+    <row- class="home">
+        home nè
+    </row->
 </template>
 
 <script>
-export default {};
+export default {
+    components: {
+        ...'button',
+        ...'col',
+        ...'row',
+    },
+};
 </script>
+<style lang="scss">
+.home {
+    background: white;
+}
+</style>
---
+++ src/client/script/vue/routes/point-of-sale.vue
@@ -0,0 +1,87 @@
+<template>
+    <row- class="point-of-sale light" >
+        <col- class="left full">
+            <row- size="40" 
+                  class="title">
+                <space-/>
+                <row- size="40" 
+                      class="row">
+                    <line-/>
+                    <input- v-model="search" 
+                            type="text"
+                            icon=""
+                            class="search-box base"
+                            placeholder="Tìm kiếm"> 
+                        <col- class="dropdown" 
+                              @click.native="log">
+                            a
+                        </col->
+                    </input->
+                    <line-/>
+                </row->
+                <button- icon=""/>
+            </row->
+            <line-/>
+            <space-/>
+        </col->
+        <col- class="right">
+            <!--//-->
+        </col->
+    </row->
+</template>
+
+<script>
+export default {
+    components: {
+        ...'col',
+        ...'row',
+        ...'line',
+        ...'button',
+        ...'label',
+        ...'layer',
+        ...'input',
+        ...'space',
+    },
+    data() {
+        return {
+            search: '',
+        };
+    },
+    watch: {
+        search(value) {
+            console.log(value);
+        },
+    },
+};
+</script>
+<style lang="scss">
+$padding: 15px;
+.point-of-sale {
+    padding: $padding;
+    > * {
+        box-shadow: 0 0 5px rgba(black, 0.2);
+        background: white;
+        border-radius: 3px;
+        margin: $padding;
+        &.left {
+            > .title {
+                > .search-box {
+                    flex: 0.5;
+                    &.focus > .dropdown,
+                    > .dropdown:hover {
+                        visibility: visible;
+                    }
+                    .dropdown {
+                        // visibility: collapse;
+                        top: calc(100% + 10px);
+                        background: white;
+                    }
+                }
+            }
+        }
+        &.right {
+            width: 300px;
+        }
+    }
+}
+</style>
