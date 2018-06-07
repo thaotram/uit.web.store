e6fb9008a045610a6abd25e0f3d82d07cde05c66
Lê Thị Thảo Trâm <sawa.kisei@gmail.com>
2018-06-07 14:27:21 +0700

    Update App.js

--- src/server/app.js
+++ src/server/app.js
@@ -4,6 +4,7 @@ import chalk from 'chalk';
 import moment from 'moment';
 import express from 'express';
 import SocketIO from 'socket.io';
+import AppDatabase from './database/database';
 import AppConfig from './config/config';
 import AppExpress from './express/express';
 import AppSocket from './socket/socket';
@@ -13,20 +14,27 @@ const port = process.env.PORT || 80;
 const server = http.createServer(app);
 const io = SocketIO(server);
 
-AppConfig(app, io);
-AppExpress(app);
-AppSocket(io);
-server.listen(port, () => {
-    log(
-        {
-            Environment: process.env.NODE_ENV || 'Unknown Environment',
-            Url: `http://127.0.0.1:${port}`,
-            Time: `${moment().format('hh:mm:ss')}`,
-        },
-        {
-            title: 'App.js',
-            color: chalk.blue,
-            length: 55,
-        },
-    );
-});
+(async function() {
+    await AppDatabase(app);
+
+    AppConfig(app, io);
+    AppExpress(app);
+    AppSocket(io);
+    
+    server.listen(port, () => {
+        log(
+            {
+                Environment:
+                    process.env.NODE_ENV ||
+                    'Unknown Environment',
+                Url: `http://127.0.0.1:${port}`,
+                Time: `${moment().format('hh:mm:ss')}`,
+            },
+            {
+                title: 'App.js',
+                color: chalk.blue,
+                length: 55,
+            },
+        );
+    });
+})();
