02f3235bb0f30d6f7709a9760c529d1b04ef65f2
Lê Thị Thảo Trâm <sawa.kisei@gmail.com>
2018-06-09 21:26:57 +0700

    Add try catch to facebook

--- src/server/database/utils/Facebook.js
+++ src/server/database/utils/Facebook.js
@@ -3,8 +3,14 @@ import fb from 'fb';
  * @param {String} accessToken
  */
 export async function getUserInfo(accessToken) {
-    return await fb.api('me', {
-        fields: ['id', 'name'],
-        access_token: accessToken,
-    });
+    try {
+        return await fb.api('me', {
+            fields: ['id', 'name'],
+            access_token: accessToken,
+        });
+    } catch (e) {
+        return {
+            error: e,
+        };
+    }
 }
