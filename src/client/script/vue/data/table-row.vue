81ceed3c2f1d9d78a0fe137d5d060981d972beb0
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2018-06-10 22:27:22 +0700

    Create Table components

---
+++ src/client/script/vue/data/table-row.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="table-row row">
+        <slot/>
+    </div>
+</template>
+<script>
+import style from 'style';
+export default {
+    mounted() {
+        const dpi = window.devicePixelRatio;
+        style({
+            '.table-row > *:not(:first-child)': {
+                borderLeftWidth: `${1 / dpi}px`,
+            },
+            '.content > .table-row': {
+                borderBottomWidth: `${1 / dpi}px`,
+            },
+        });
+    },
+};
+</script>
+<style lang="scss">
+.table-row {
+    font-size: 14px;
+    display: flex;
+    > div {
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        box-sizing: content-box;
+        height: 25px;
+        line-height: 25px;
+        padding: 10px 15px;
+    }
+    > input {
+        outline: 0;
+        border: none;
+        min-width: 10px;
+    }
+
+    > div,
+    input {
+        padding: 10px 15px;
+        &:not(:first-child) {
+            border-left-style: solid;
+            border-left-color: $light-line-color;
+        }
+    }
+}
+
+.content > .table-row {
+    border-bottom-style: solid;
+    border-bottom-color: $light-line-color;
+}
+</style>
---
+++ src/client/script/vue/data/table-view.vue
@@ -0,0 +1,51 @@
+<template>
+    <div :uid="_uid" 
+         class="table-view col">
+        <div class="header">
+            <slot name="header"/>
+        </div>
+        <line-/>
+        <list- class="content">
+            <slot name="content"/>
+        </list->
+    </div>    
+</template>
+<script>
+import { initialize } from 'style';
+import { getTableStyle } from './table-utils';
+
+export default {
+    components: {
+        ...'line',
+        ...'list',
+    },
+    props: {
+        size: {
+            type: Array,
+            default: () => [],
+        },
+    },
+    updated() {
+        initialize({
+            group: 'table',
+            overwrite: true,
+            style: getTableStyle(this),
+        });
+    },
+};
+</script>
+<style lang="scss">
+.table-view {
+    display: flex;
+    flex-direction: column;
+    > .header {
+        background: #e6e6e6;
+        >.table-row > span {
+            width: 12px;
+        }
+    }
+    > .content {
+        overflow-y: scroll;
+    }
+}
+</style>
