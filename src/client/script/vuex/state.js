c5aa96eefea53628a0168046150eca01b4bdcd62
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2018-06-13 04:58:24 +0700

    Add App Const values

--- src/client/script/vuex/state.js
+++ src/client/script/vuex/state.js
@@ -1,4 +1,9 @@
 export default {
+    app: {
+        name: 'Tiệm sách thông minh',
+        address: 'Khu phố 6, Phường Linh Trung, Quận Thủ Đức, Thành phố Hồ Chí Minh',
+        phone: '(028) 372 51993',
+    },
     user: {
         id: '12312535123',
         name: 'Phạm Hồng Phúc',
