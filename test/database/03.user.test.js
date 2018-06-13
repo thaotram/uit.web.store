7983a706a05c76d2577271afde2e3953191ca521
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2018-06-13 14:27:13 +0700

    Update Test

--- test/database/03.user.test.js
+++ test/database/03.user.test.js
@@ -1,19 +1,18 @@
 import { assert } from 'chai';
 import { filename, itname } from '../utils/utils';
-import { getUserInfo } from '../../src/server/database/utils/Facebook';
+import { getUserInfo } from '../../src/server/credential/facebook.server';
 import database, { User } from '../../src/server/database/database';
 import nock from 'nock';
 import { FacebookApiException } from 'fb';
 
 describe(filename(__filename), function() {
     this.timeout(1000000);
-    const access_token = 'THIS_IS_A_FAKE_ACCESS_TOKEN';
+    const token = 'THIS_IS_A_FAKE_ACCESS_TOKEN';
+    const sessionId = 'THIS_IS_A_FAKE_SESSION_ID';
 
     beforeEach(function() {
         nock('https://graph.facebook.com')
-            .get(
-                `/v3.0/me?fields=%5B%22id%22%2C%22name%22%5D&access_token=${access_token}`,
-            )
+            .get(`/v3.0/me?fields=%5B%22id%22%2C%22name%22%5D&access_token=${token}`)
             .reply(200, {
                 id: 715380908507851,
                 name: 'Lê Thị Thảo Trâm',
@@ -21,17 +20,21 @@ describe(filename(__filename), function() {
     });
 
     it(itname('.getUserInfo()', 'Lấy thông tin người dùng'), async function() {
-        const info = await getUserInfo(access_token);
+        const info = await getUserInfo(token);
         assert.equal(info.name, 'Lê Thị Thảo Trâm');
     });
 
-    it(itname('User.getByAccessToken()', 'Tạo thông tin người dùng'), async function() {
+    it(itname('User.get()', 'Tạo thông tin người dùng'), async function() {
         const realm = await database();
         try {
-            const user = await User.getByAccessToken(realm, access_token);
+            const req = {
+                id: 715380908507851,
+                token,
+            };
+            const user = await User.get(realm, req, sessionId);
             assert.equal(user.name, 'Lê Thị Thảo Trâm');
         } catch (e) {
-            console.log(e);
+            // console.log(e);
             assert.equal(e.error, `Can't get user info`);
         }
     });
