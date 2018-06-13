c31696e3b86e8257c4098544b8f12fcfffbcd425
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2018-06-13 12:56:30 +0700

    temp

--- src/client/script/modules/index.js
+++ src/client/script/modules/index.js
@@ -1,3 +1,14 @@
 export { default as toEnglish } from './src/toEnglish';
 export { default as toMoney } from './src/toMoney';
 export { default as found } from './src/found';
+export { default as facebookInitialize } from './src/facebook.client';
+
+export default {
+    /**
+     * @returns {FB}
+     */
+    get FB() {
+        if (window.FB) return window.FB;
+        else throw 'Không thể lấy đối tượng FB khi nó chưa được khởi tạo';
+    },
+};
--- src/client/script/script.js
+++ src/client/script/script.js
@@ -2,11 +2,24 @@ import Vue from 'vue';
 import router from './router';
 import store from './vuex/store';
 import app from './vue/app/app.vue';
+import { socket } from './socket/socket';
 import('../style/index.scss');
+
+import modules, { facebookInitialize } from './modules';
 
 new Vue({
     el: '#app',
     router,
     store,
+    async beforeCreate() {
+        await facebookInitialize();
+        const FB = modules.FB;
+        FB.getLoginStatus(response => {
+            if (response.status === 'connected') {
+                console.log(response);
+            }
+        });
+        socket;
+    },
     render: h => h(app, { ref: 'app' }),
 });
